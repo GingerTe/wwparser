6d6630c99c9bc80f26c1a95f205f7b4034dd2e06
Maria Prudyvus <maria.prudyvus@gmail.com>
2018-11-16 00:33:47 +0300

    пишет в базу

---
+++ model.py
@@ -0,0 +1,39 @@
+import json
+
+from sqlalchemy import Column, Integer, String, DateTime, Text, JSON
+from sqlalchemy.event import listens_for
+from sqlalchemy.ext.declarative import declarative_base
+from engine import engine
+
+Base = declarative_base()
+
+
+class Data(Base):
+    __tablename__ = 'data'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    user = Column(String, index=True)
+    txt = Column(Text, index=True)
+    location = Column(String, index=True)
+    km = Column(Integer, index=True)
+    received = Column(Text)
+    bonus = Column(Text)
+    zone = Column(String)
+
+    def __init__(self, **kwargs):
+        for attr in kwargs:
+            self.__setattr__(attr, kwargs.get(attr))
+
+    # def __repr__(self):
+    #     return "<Data('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
+
+
+@listens_for(Data, 'before_insert')
+def do_stuff(mapper, connect, target: Data):
+    # target is an instance of Table
+    target.bonus = json.dumps(target.bonus, ensure_ascii=False, indent=2)
+    target.received = json.dumps(target.received, ensure_ascii=False, indent=2)
+
+
+# Создание таблицы
+Base.metadata.create_all(engine)
--- parser.py
+++ parser.py
@@ -1,154 +1,162 @@
 # coding: utf-8
+import datetime
 import json
 import os
 import re
-from copy import copy
 
+from sqlalchemy.orm import sessionmaker
+
+from engine import engine
+from model import Data
 import lxml.html as html
 
-# Путь к папке с логами телеги
-DIRECTORY_WITH_LOGS = r'data'
-# Название файла с результатом
-RESULT_FILE_NAME = 'all.json'
-
-res = {}
-params_regexp = re.compile(r'.*\d+/\d+ 🍗\d+% 🔋\d+/\d+ 👣(\d+)+км(.*)')
-
-skip_line_anchors = 'Найдено', 'Ты ранен', 'Потеряно', 'Проебано', 'Ты потерял', 'будет проводиться 👊Рейд', \
-                    '/help', 'Ты можешь продолжить путь в Темной зоне', 'Найден дрон', 'Ты заработал', \
-                    'Ты покидаешь Темную зону'
-
-skip_block_anchors = 'Ты тоже хлам собираешь', 'Твой путь преградил', 'Проводник', 'Старьёвщик', \
-                     'Хламосборщик', 'Таинственный незнакомец', 'Во время вылазки на тебя напал', \
-                     'Ты проиграл в этой схватке', \
-                     'Безумный старик', 'Они все мертвы', 'Тебя буквально размазали', \
-                     'Недалеко от себя ты заметил какого-то человека', 'Недалеко ты заметил какого-то человека', \
-                     'Белое гетто', 'Выбери одну из фракций'
+DATE_FORMAT = '%d.%m.%Y %H:%M:%S'
 
 
-def check_skipped(l, type_='line'):
-    if type_ == 'all':
-        skip_list = skip_block_anchors
-    else:
-        skip_list = skip_line_anchors
-    for si in skip_list:
-        if si in l:
-            return True
-    return False
+class Parser:
+    SKIP_LINE_ANCHORS = 'Найдено', 'Ты ранен', 'Потеряно', 'Проебано', 'Ты потерял', 'будет проводиться 👊Рейд', \
+                        '/help', 'Ты можешь продолжить путь в Темной зоне', 'Найден дрон', 'Ты заработал', \
+                        'Ты покидаешь Темную зону', 'Тебе удалось избежать схватки, используя такое модное ' \
+                                                    'молодежное направление, как "бег"'
 
+    SKIP_BLOCK_ANCHORS = 'Ты тоже хлам собираешь', 'Твой путь преградил', 'Проводник', 'Старьёвщик', \
+                         'Хламосборщик', 'Таинственный незнакомец', 'Во время вылазки на тебя напал', \
+                         'Ты проиграл в этой схватке', \
+                         'Безумный старик', 'Они все мертвы', 'Тебя буквально размазали', \
+                         'Недалеко от себя ты заметил какого-то человека', 'Недалеко ты заметил какого-то человека', \
+                         'Белое гетто', 'Выбери одну из фракций'
 
-for user in os.listdir(DIRECTORY_WITH_LOGS):
-    if user == 'platonoff':
-        continue
-    print(user)
-    user_dir = os.path.join(DIRECTORY_WITH_LOGS, user)
-    for name in os.listdir(user_dir):
-        if not name.endswith('.html'):
-            continue
+    PARAMS_REGEXP = re.compile(r'.*\d+/\d+ 🍗\d+% 🔋\d+/\d+ 👣(\d+)+км(.*)')
 
-        with open(os.path.join(user_dir, name), encoding='utf8') as f:
-            data = f.read()
+    def __init__(self, log_dir='data'):
+        self.log_dir = log_dir
+        self.current_line = ''
+        self.doc = None
 
-        doc = html.document_fromstring(data)
-        for br in doc.xpath("*//br"):
+        session = sessionmaker()
+        session.configure(bind=engine)
+        self.session = session()
+
+    def _fix_br(self):
+        for br in self.doc.xpath("*//br"):
             br.tail = "\n" + br.tail if br.tail else "\n"
-        msgs = doc.find_class('text')
 
-        for msg in msgs:
+    def check_skipped(self, type_='line'):
+        if type_ == 'all':
+            skip_list = self.SKIP_BLOCK_ANCHORS
+        else:
+            skip_list = self.SKIP_LINE_ANCHORS
+
+        for si in skip_list:
+            if si in self.current_line:
+                return True
+        return False
+
+    def parse_all(self):
+        self.session.query(Data).delete()
+        self.session.commit()
+        for user in os.listdir(self.log_dir):
+            print(user)
+            user_dir = os.path.join(self.log_dir, user)
+            for name in os.listdir(user_dir):
+                if not name.endswith('.html'):
+                    continue
+                self.parse(os.path.join(user_dir, name), user)
+
+    def parse(self, file_name, user):
+        with open(file_name, encoding='utf8') as f:
+            data = f.read()
+        self.doc = html.document_fromstring(data)
+        self._fix_br()
+        body = self.doc.find_class('body')
+
+        for block in body:
+            msg_date = self._get_date(block)
+
+            msg = self._get_msg(block)
+
+            if not (msg_date and msg):
+                continue
+            data = Data(user=user, date=msg_date)
 
             content = msg.text_content().strip().split('\n')
 
-            location = None
-            km = None
-            txt = []
-            received = []
-            bonus = []
-            f_skip_line = False
+            data.txt = []
+            data.received = []
+            data.bonus = []
             f_skip_block = False
 
-            for index, line in enumerate(content):
-                line = line.strip()
-                if not line:
+            for index, self.current_line in enumerate(content):
+                self.current_line = self.current_line.strip()
+                if not self.current_line:
                     continue
                 # Первая строка - локация
-                elif not location:
-                    location = line
+                elif not data.location:
+                    data.location = self.current_line
                 # Если строка параметров
-                elif params_regexp.match(line):
-                    km = params_regexp.match(line).group(1)
+                elif self.PARAMS_REGEXP.match(self.current_line):
+                    km = self.PARAMS_REGEXP.match(self.current_line).group(1)
+                    if not km:
+                        break
+                    data.km = int(km)
                 # Если получили локацию, дальше должен идти км
                 # Дополнительно проверяем, не является ли блок пропускаемым
-                elif location and not km or check_skipped(line, 'all'):
+                elif data.location and not data.km or self.check_skipped('all'):
                     f_skip_block = True
                     break
                 # Проверка, не нужно ли пропустить строку
-                elif check_skipped(line, 'line'):
+                elif self.check_skipped('line'):
                     continue
                 # Обработка полученного хлама
-                elif line.startswith('Получено'):
-                    s = line.split(':')
-                    received.append(s[1].strip())
+                elif self.current_line.startswith('Получено'):
+                    s = self.current_line.split(':')
+                    data.received.append(s[1].strip())
                     # if len(s) > 1:
                     #     num = ''.join(i for i in line if i.isdigit())
                     #     get[-1]['num'] = int(num) if num else 1
                 # Обработка полученных бонусов
-                elif line.startswith('Бонус'):
-                    line = line[7:]
-                    if '❤' in line:
+                elif self.current_line.startswith('Бонус'):
+                    self.current_line = self.current_line[7:]
+                    if '❤' in self.current_line:
                         continue
                     else:
-                        bonus.append(line)
+                        data.bonus.append(self.current_line)
 
                 else:
-                    txt.append(line)
+                    data.txt.append(self.current_line)
             # Если получили флаг пропустить блок, или не получили км, или не получили текстовку
-            if f_skip_block or not km or not txt:
+            if f_skip_block or not (data.km and data.txt):
                 continue
-            km = int(km)
-            txt = ' '.join(txt)
+            data.txt = ' '.join(data.txt)
 
             # Убираем владельца локации
-            if '(' in location:
-                location = location[:location.index('(')]
+            if '(' in data.location:
+                data.location = data.location[:data.location.index('(')]
             # Определяем зону
-            zone = 'safe zone'
-            if location.startswith('🚷'):
-                zone = 'dark zone'
-                location = location[2:]
+            data.zone = 'safe'
+            if data.location.startswith('🚷'):
+                data.zone = 'dark'
+                data.location = data.location[2:]
 
-            # Создаем ключи, если надо
-            if txt not in res:
-                res[txt] = {}
-            if zone not in res[txt]:
-                res[txt][zone] = {'received': {}, 'bonus': []}
-            # res[txt] = res.get(txt, {})
-            # res[txt][zone] = res.get(zone, {'get': [], 'location': [], 'km': [], 'bonus': []})
+            self.session.add(data)
+        self.session.commit()
 
-            block = res[txt][zone]
-            if received:
-                received = ', '.join(received)
-                block['received'][received] = block['received'].get(received, 0) + 1
-            if bonus:
-                block['bonus'].append(bonus)
+    @staticmethod
+    def _get_msg(block):
+        msg = block.find_class('text')
+        if msg:
+            msg = msg[0]
+            return msg
 
-            # if km not in block['km']:
-            #     block['km'].append(km)
-            #
-            # if location not in block['location']:
-            #     block['location'].append(location)
+    @staticmethod
+    def _get_date(block):
+        msg_date = None
+        date_element = block.find_class('date')
+        if date_element:
+            date_string = date_element[0].attrib['title']
+            msg_date = datetime.datetime.strptime(date_string, DATE_FORMAT)
+        return msg_date
 
-# Убираем пустые списки
-for data in res.values():
-    for i in data.values():
-        for key in copy(i):
-            if not i[key]:
-                del i[key]
-            elif isinstance(i[key], list):
-                i[key].sort()
 
-with open(RESULT_FILE_NAME, 'w', encoding='utf8') as f:
-    json.dump(res, f, indent=2, ensure_ascii=False)
-
-with open('text.txt', 'w', encoding='utf8') as f:
-    for index, key in enumerate(res, 1):
-        f.write('{}. {}\n\n'.format(index, key))
+if __name__ == '__main__':
+    Parser().parse_all()
